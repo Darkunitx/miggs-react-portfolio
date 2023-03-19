bd768597627fe3ddad1e4b79f906356ffa5129dd
Darkunitx <darkunitx@live.com>
2023-03-18 23:36:17 -0500

    started header/nav bar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './index.css';
-function Header(){
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+
+function Header() {
     return (
-        <h1>Hello World</h1>
-    )
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand href="#Home">Miguel's React Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#Home">About Me</Nav.Link>
+                        <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="#Contact">Contact</Nav.Link>
+                        <Nav.Link href="#Resume">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
 
 export default Header;
